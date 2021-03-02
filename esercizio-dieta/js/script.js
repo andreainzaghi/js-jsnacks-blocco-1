2ac40dc9ca104bce582b722f29e47d56c1a303b8
Andrea <andrea3aprile99@gmail.com>
2021-03-02 22:17:03 +0100

    x

---
+++ esercizio-dieta/js/script.js
@@ -0,0 +1,25 @@
+
+var invitati = ["yogurt","pollo","albume","cioccolato","farro","broccoli","carote","mela","olio","riso","banana"];
+var arr2 = ["52","156","43","515","335","24","41","52","885","356","93"];
+var cibo = parseInt(prompt("Benvenuto! cosa hai mangiato?"));
+
+
+var nomeg=0;
+
+// 2. Confrontarla con le altre email
+
+for (var i = 0; i < invitati.length; i++) {
+  if (cibo.toLowerCase() == invitati[i].toLowerCase()) {
+      console.log("i");
+      nomeg = 1;
+  }
+}
+
+if (nomeg == 1) {
+  var peso = prompt("In che quantita(gr.)?");
+  var sum = 0;
+  // for(var i=0; i< invitati.length; i++) {
+    sum += arr2[i]*peso;
+  // }
+}
+ document.getElementById("messaggio").innerHTML =" la somma e\' :" + sum ;
