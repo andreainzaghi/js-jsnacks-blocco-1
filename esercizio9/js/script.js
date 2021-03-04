42a80ea994db5bea4d8e6ff8f0aaf18fc4d61fb9
Andrea <andrea3aprile99@gmail.com>
2021-03-04 11:41:17 +0100

    d

--- esercizio9/js/script.js
+++ esercizio9/js/script.js
@@ -1,8 +1,24 @@
 
+ // var somma =0;
+ // for (var i = 0; i < 5; i++){
+ //   numero = parseInt(prompt('Inserisci un numero'));
+ //   somma+=numero;
+ // }
+ //
+ // document.write(somma);
+ //
+ //
+
+
+
+
+ // ...................................................
  var somma =0;
- for (var i = 0; i < 5; i++){
+ var i = 0;
+while (  i < 5 ) {
    numero = parseInt(prompt('Inserisci un numero'));
-   somma+=numero;
- }
+     somma+=numero;
+  i++;
+}
 
  document.write(somma);
