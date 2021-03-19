9e0d8ee60460e3331d728b54275a1897e8aaf2bb
Andrea <andrea3aprile99@gmail.com>
2021-03-19 10:03:54 +0100

    m

---
+++ esercizio12/js/script.js
@@ -0,0 +1,16 @@
+var bicio = [
+	{
+bici:"rossella",
+peso:15000,
+},
+{
+  bici:"bianchi",
+  peso:14000,
+}
+];
+
+if(bicio[0].peso < bicio[1].peso){
+  console.log(bicio[0].bici);
+}else{
+  console.log(bicio[1].bici);
+}
